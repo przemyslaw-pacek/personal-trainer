a9f4520461ce2d4e593a8c458ba790bc6ef49fd2
Przemyslaw Pacek <ppacek@op.pl>
2025-11-21 18:09:32 +0100

    improved component form

--- src/app/home/form/form.ts
+++ src/app/home/form/form.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
@@ -13,12 +14,17 @@ export class Form {
   email = '';
   message = '';
 
-  isSent = false;
+  isSent = signal(false);
 
   sendMessage() {
-    this.isSent = true;
+    this.isSent.set(true);
+
     this.name = '';
     this.email = '';
     this.message = '';
+
+    setTimeout(() => {
+      this.isSent.set(false);
+    }, 5000);
   }
 }
