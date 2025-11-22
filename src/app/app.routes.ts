f9fb67cdbd6ef0440cd95f39ba95c047e83248dc
Przemyslaw Pacek <ppacek@op.pl>
2025-11-22 21:50:50 +0100

    changed names

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Achievements } from './achievements/achievements';
-import { Transformations } from './transformations/transformations';
+import { Transform } from './transform/transform';
 import { Gallery } from './gallery/gallery';
 import { About } from './about/about';
 import { Training } from './training/training';
@@ -10,7 +10,7 @@ import { Terms } from './terms/terms';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'achievements', component: Achievements },
-  { path: 'transformations', component: Transformations },
+  { path: 'transform', component: Transform },
   { path: 'gallery', component: Gallery },
   { path: 'about', component: About },
   { path: 'training', component: Training },
