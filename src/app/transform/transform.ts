e993a935ffbb8580fa182bb9e579e67c6c89589c
Przemyslaw Pacek <ppacek@op.pl>
2025-11-22 21:48:37 +0100

    added transform component

---
+++ src/app/transform/transform.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transform',
+  imports: [],
+  templateUrl: './transform.html',
+  styleUrl: './transform.css',
+})
+export class Transform {}
