7db8a241290745be8b71c88a06415810e5e2b3f1
Przemyslaw Pacek <ppacek@op.pl>
2025-12-07 13:15:02 +0100

    data file created

---
+++ src/app/data.ts
@@ -0,0 +1,46 @@
+export interface CardItem {
+  title: string;
+  text: string;
+}
+
+export const cards: CardItem[] = [
+  {
+    title: 'Wymarzona sylwetka',
+    text: `Pomogę Ci zrzucić zbędne kilogramy dzięki systematyznej pracy nad poszczególnymi partiami mięśni. 
+      Wymarzona figura jest w zasięgu ręki.`,
+  },
+  {
+    title: 'Poprawa życia i zdrowia',
+    text: `Zbilansowana dieta dostosowana indywidualnie do Ciebie w połączeniu z regularną aktywnością fizyczną 
+      to klucz do doskonałego zdrowia oraz przedłużenie życia.`,
+  },
+  {
+    title: 'Większy poziom energii',
+    text: `Dzięki systematycznej aktywność fizycznej połączonej 
+      z odpowiednim odżywianiem poczujesz więcej energii do życia, więcej motywacji.`,
+  },
+  {
+    title: 'Lepsza kondycja psychiczna',
+    text: `Trening połączony z odpowiednią dietą obniża poziom stresu, 
+      co w znacznym stopniu wpływa na kondycję psychiczną w myśl przysłowia "W zdrowym ciele zdrowy duch".`,
+  },
+];
+
+export const feats: CardItem[] = [
+  {
+    title: '8+',
+    text: 'lat doświadczenia treningowego',
+  },
+  {
+    title: '500 tys.',
+    text: 'obserwujących w social mediach',
+  },
+  {
+    title: '3 093+',
+    text: 'podopiecznych którzy mi zaufali',
+  },
+  {
+    title: '98%',
+    text: 'skuteczności i zadowolenia',
+  },
+];
